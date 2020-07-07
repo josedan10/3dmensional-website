cc53b000bde33924586e2b2bfeab6a5e7b5d3090
José Daniel  Quintero <josedanq100@gmail.com>
2020-07-07 09:35:19 -0400

    build: :heavy_plus_sign: laravel mix svg

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,4 +1,4 @@
-const mix = require('laravel-mix');
+const mix = require('laravel-mix')
 
 /*
  |--------------------------------------------------------------------------
@@ -11,5 +11,15 @@ const mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/js/app.js', 'public/js')
-    .sass('resources/sass/app.scss', 'public/css');
+mix
+  .js('resources/js/app.js', 'public/js')
+  .sass('resources/sass/app.scss', 'public/css')
+  .svgVue({
+    svgPath: 'public/icons',
+    extract: false,
+    svgoSettings: [
+      { removeTitle: true },
+      { removeViewBox: true },
+      { removeDimensions: false }
+    ]
+  })
