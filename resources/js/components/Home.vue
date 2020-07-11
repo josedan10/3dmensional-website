89ead848b396289b60c554cb8791da8a3af3721d
Rafael Calles <raphacall@gmail.com>
2020-07-11 15:55:47 -0400

    feat: timer is working

--- resources/js/components/Home.vue
+++ resources/js/components/Home.vue
@@ -1,6 +1,6 @@
 <template>
-    <div>
-        
+    <div v-show="this.timer.int">
+     {{ timer.days }}:{{ timer.hours }}:{{ timer.mins }}:{{ timer.secs }}
     </div>
 </template>
 
@@ -11,9 +11,42 @@ export default {
       secs: 0,
       mins: 0,
       hours: 0,
-      days: 0
+      days: 0,
+      int: true
     }
-  })
+  }),
+  methods: {
+    StartTimer: function () {
+      this.timer.days = 10
+      setInterval(this.tick, 1000)
+    },
+    tick: function() {
+      if (this.timer.days !== 0 && this.timer.hours == 0 && this.timer.mins == 0 && this.timer.secs == 0) {
+        this.timer.days -= 1
+        this.timer.hours = 23
+        this.timer.mins = 59
+        this.timer.secs = 59
+      }
+      else if (this.timer.secs !== 0) {
+        this.timer.secs -= 1
+      }
+      else if (this.timer.mins > 0 && this.timer.secs == 0) {
+        this.timer.mins -= 1
+        this.timer.secs = 59
+      }
+      else if (this.timer.hours > 0 && this.timer.mins == 0 && this.timer.secs == 0) {
+        this.timer.hours -= 1 
+        this.timer.mins = 59
+        this.timer.secs = 59
+      }
+      else if (this.timer.days == 0 && this.timer.hours == 0 && this.timer.mins == 0 && this.timer.secs == 0) {
+        this.timer.int = !this.timer.int
+      }
+    },
+  },
+  mounted () {
+    this.StartTimer()
+  },
 }
 </script>
 
